e22a483657c348160403062a604bb477a6577e0d
prabhat dubey <dprabhat670@gmail.com>
2023-06-08 17:02:40 +0530

    🔥 chore(HomeButton.js): delete HomeButton component ✨ feat(OptionButton.js): add OptionButton component 🎨 style(OptionButton.js): add styles to OptionButton component 🚀 perf(Home.js): add horizontal ScrollView for OptionButton components

---
+++ src/components/OptionButton.js
@@ -0,0 +1,26 @@
+import { View, Text, StyleSheet } from 'react-native';
+import React from 'react';
+
+export default function OptionButton() {
+  return (
+    <View style={styles.container}>
+      <Text>HomeButton</Text>
+    </View>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    borderRadius: '20%',
+    width: 8,
+    height: 4,
+    backgroundColor: 'yellow',
+    position: 'relative',
+  },
+  optionText: {
+    color: 'black',
+    fontSize: 8,
+    position: 'relative',
+    top: '50%',
+    left: '50%',
+  },
+});
--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,14 +2,45 @@ import { View, Text, ScrollView, StyleSheet } from 'react-native';
 import React from 'react';
 import Card from '../components/Card';
 import Megahomecard from '../components/Megahomecard';
+import OptionButton from '../components/OptionButton';
 export default function Home() {
+  const options = [
+    {
+      id: 1,
+      title: 'All',
+      bg: '',
+    },
+    {
+      id: 2,
+      title: '',
+    },
+    {
+      id: 3,
+      title: '',
+    },
+    {
+      id: 4,
+      title: '',
+    },
+    {
+      id: 5,
+      title: '',
+    },
+  ];
+
   return (
     <>
       <ScrollView>
         <View style={{ marginVertical: 10 }}>
           <Megahomecard />
         </View>
-        <Text style={styles.fetchHeader}>Articles & Blogs </Text>
+        <View>
+          <Text style={styles.fetchHeader}>Articles & Blogs </Text>
+        </View>
+        <ScrollView horizontal={true}>
+          <OptionButton />
+        </ScrollView>
+
         <Card />
       </ScrollView>
     </>
